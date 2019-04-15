4d6f9ecd29c2001ceb85d370207593ead357f15b
zhouLion <748583403@qq.com>
2019-04-15 10:05:20 +0800

    登录、滑块组件，apply列表组件

--- src/views/DealApplication.vue
+++ src/views/DealApplication.vue
@@ -1,9 +1,9 @@
 <template>
   <v-layout column wrap>
-    <v-tabs fixed-tabs v-model="model">
+    <v-tabs v-model="model">
       <v-tab to="/dealapplication/fromme">我的申请</v-tab>
       <v-tab to="/dealapplication/tome">待我审核</v-tab>
-      <v-tab to="/dealapplication/history">历史记录</v-tab>
+      <!-- <v-tab to="/dealapplication/history">历史记录</v-tab> -->
     </v-tabs>
     <!-- <keep-alive> -->
     <router-view></router-view>
--- src/views/DealApplication/ToMe.vue
+++ src/views/DealApplication/ToMe.vue
@@ -1,14 +1,120 @@
 <template>
   <div>
-    待我审核
+    <Applications
+      apptitle="待我审核"
+      :loading="onloading"
+      :data="applicationToMe"
+    >
+      <v-btn icon @click="fetchApplications" slot="headerAction">
+        <v-icon>refresh</v-icon>
+      </v-btn>
+      <div slot="action" slot-scope="{ props, array }">
+        <v-tooltip top color="error">
+          <v-btn
+            slot="activator"
+            flat
+            :loading="prevId == props.item.id"
+            icon
+            small
+            color="error"
+            @click="returnAssign(props)"
+          >
+            <v-icon>assignment_return</v-icon>
+          </v-btn>
+          驳回
+        </v-tooltip>
+        <v-tooltip top color="success">
+          <v-btn
+            slot="activator"
+            flat
+            :loading="prevId == props.item.id"
+            icon
+            small
+            color="success"
+            @click="turnInAssign(props)"
+          >
+            <v-icon>assignment_turned_in</v-icon>
+          </v-btn>
+          通过
+        </v-tooltip>
+      </div>
+    </Applications>
+    <v-snackbar top v-model="showSnackbar">
+      {{ operate }}
+      <v-btn flat color="primary" @click.native="rebackPrevOperate">撤销</v-btn>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
+import Applications from "../../components/table/Applications";
+import apply from "../../api/applications";
 export default {
-  name: "",
+  name: "i_deal",
+  components: {
+    Applications
+  },
   data() {
-    return {};
+    return {
+      applicationToMe: [],
+      onloading: false,
+      showSnackbar: false,
+      operate: null,
+      prevId: null
+    };
+  },
+  mounted() {
+    this.fetchApplications();
+  },
+  watch: {
+    // showSnackbar(val) {
+    //   let { operate, prevId } = this;
+    //   if (val == false && prevId) {
+    //     console.log("执行操作", { operate, prevId });
+    //     setTimeout(() => {
+    //       this.operate = null;
+    //       this.prevId = null;
+    //     }, 100);
+    //   }
+    // }
+  },
+  methods: {
+    // rebackPrevOperate() {
+    //   this.operate = null;
+    //   this.prevId = null;
+    //   this.showSnackbar = false;
+    // },
+    returnAssign({ item }) {
+      // let prevId = item.id;
+      // this.showSnackbar = false;
+      // this.operate = "驳回操作";
+      // this.prevId = prevId;
+      // this.$nextTick(() => {
+      //   this.showSnackbar = true;
+      // });
+    },
+    turnInAssign({ item }) {
+      // let prevId = item.id;
+      // this.showSnackbar = false;
+      // this.operate = "通过操作";
+      // this.prevId = prevId;
+      // this.$nextTick(() => {
+      //   this.showSnackbar = true;
+      // });
+    },
+    fetchApplications() {
+      this.onloading = true;
+      apply
+        .getApplyFromCompany({
+          path: "Root"
+        })
+        .then(data => {
+          this.applicationToMe = data.data;
+        })
+        .finally(() => {
+          this.onloading = false;
+        });
+    }
   }
 };
 </script>
