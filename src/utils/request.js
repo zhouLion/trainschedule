a173c63cefc9c8ef3fc9749d9cb110c41c08356e
汉广 <liuchangm7j@vip.qq.com>
2019-04-15 09:58:26 +0800

    new 支持跨域
    services.AddCors(options =>
                {
    	            options.AddPolicy(MyAllowSpecificOrigins,
    		            builder =>
    		            {
    			            builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed((x) =>
    				            {
    					            return true;
    				            });
    		            });
                });

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,25 +1,66 @@
-import axios from "axios";
-import config from "../app.config"
-
-const baseUrl = config.baseUrl;
-let request = axios.create({
-  baseURL: baseUrl,
-  // transformRequest?: AxiosTransformer | AxiosTransformer[];
-  // transformResponse?: AxiosTransformer | AxiosTransformer[];
-  // responseType: "application/json",
-  timeout: 7000,
-  transformResponse(data) {
-    try {
-      let response = JSON.parse(data);
-      if (response.status == 0) {
-        return response.data;
-      } else {
-        return null
-      }
-    } catch (error) {
-      return null
-    }
-  }
+import axios from 'axios'
+import qs from 'qs'
+// create an axios instance
+const service = axios.create({
+  baseURL: 'https://localhost:44310/', // api 的 base_url
+  withCredentials: true, // 跨域请求时发送 cookies
+  timeout: 5000, // request timeout
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  transformRequest: [function(data) {
+    data = qs.stringify(data)
+    return data
+  }]
 })
 
-export default request
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get information such as headers or status
+   * Please return  response => response
+  */
+  /**
+   * 下面的注释为通过在response里，自定义code来标示请求状态
+   * 当code返回如下情况则说明权限有问题，登出并返回到登录页
+   * 如想通过 XMLHttpRequest 来状态码标识 逻辑可写在下面error中
+   * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
+   */
+  response => {
+    const res = response.data
+    if (res.code !== 0) {
+      this.$Message({
+        message: res.message,
+        type: 'error',
+        duration: 5 * 1000
+      })
+
+      return Promise.reject('error')
+    } else {
+      return response.data
+    }
+  },
+  error => {
+    console.log(error) // for debug
+    this.$Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
